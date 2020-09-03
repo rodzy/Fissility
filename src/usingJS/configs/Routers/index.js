6fe758484f2c4ede2ed5a8c1233b9663eb084fe7
rodzy <49137701+rodzy@users.noreply.github.com>
2020-09-03 16:59:33 -0600

    Deleted states folder and prototyping only the routing

--- src/usingJS/configs/Routers/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "withRedux",
-    question: "Do you want your app to include Redux and Redux Saga?",
-    dependencies: ["redux", "react-redux", "redux-saga"],
-    devDependencies: ["@types/react-redux", "redux-devtools-extension"],
-    packageEntries: [],
-    templates: []
-  };
