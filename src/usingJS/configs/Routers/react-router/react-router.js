634b82270b100b99958555d1e60c1647c8902656
rodzy <49137701+rodzy@users.noreply.github.com>
2020-09-03 18:31:00 -0600

    [Fix] Basic confirm for routing modules

--- src/usingJS/configs/Routers/react-router/react-router.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: "reactRouter",
-  question: "Would you like to add routing?",
-  dependencies: ["react-router-dom", "react-redux", "redux-saga"],
-  devDependencies: ["@types/react-redux", "redux-devtools-extension"],
-  packageEntries: [],
-  templates: [],
-};
